60902724f1e79c5e8eb585cfad01ea8d3dafa589
maskedarray <rao.arrn@gmail.com>
2023-08-04 10:54:52 -0400

    added correct pmu_test.c

--- software/pmu_test/pmu_test.c
+++ software/pmu_test/pmu_test.c
@@ -4,29 +4,46 @@
 #include "utils.h"
 //#define FPGA_EMULATION
 
+ 
+
 #define cfg_write(addr, val_)  (*(volatile unsigned int *)(long)(addr) = val_)
 #define cfg_read(addr)         (*(volatile unsigned int *)(long)(addr))
 
+ 
+
+// All Widths are in Bytes
 #define NUM_COUNTER 4
 #define TIMER_WIDTH 8
 #define COUNTER_WIDTH 8
 #define CONFIG_WIDTH 4
 #define NUM_ELEMENT 100
 
+ 
+
+#define INSTR_WIDTH 4
+
+ 
+
 void read_counters(int num_counter, long long unsigned int BASE_ADDR, int REG_SIZE_IN_BYTES);
 void write_counters(int num_counter, long long unsigned int BASE_ADDR, int REG_SIZE_IN_BYTES, long long int val[]);
 
+ 
+
 int main(int argc, char const *argv[]) {
+
+ 
 
   #ifdef FPGA_EMULATION
   int baud_rate = 9600;
-  int test_freq = 50000000;
+  int test_freq = 100000000;
   #else
   set_flls();
   int baud_rate = 115200;
-  int test_freq = 50000000;
+  int test_freq = 100000000;
   #endif  
   uart_set_cfg(0,(test_freq/baud_rate)>>4);
+
+ 
 
   // Pointer is char to make it byte-addressable,
   // event_sel_config does not always align by 32B boundary!
@@ -36,16 +53,37 @@ int main(int argc, char const *argv[]) {
   long long unsigned int PMU_INIT_BUDGET_BASE_ADDR  = 0x10404000 + 1*NUM_COUNTER*8 + 2*NUM_COUNTER*4;
   long long unsigned int PMU_PERIOD_REG_BASE_ADDR   = 0x10404000 + 2*NUM_COUNTER*8 + 2*NUM_COUNTER*4;
   long long unsigned int PMU_TIMER_BASE_ADDR        = 0x10404000 + 2*NUM_COUNTER*8 + 2*NUM_COUNTER*4 + 1*TIMER_WIDTH;
+  long long unsigned int PMU_INSTR_SPM_BASE_ADDR    = 0x10404000;
+
+ 
 
   long long int counter_val[]      = {0x100, 0x200, 0x300, 0x400};
-  long long int event_sel_val[]    = {0x2F, 0x3F, 0x4F, 0x5F};
+  long long int event_sel_val[]    = {0x1F002F, 0x1F003F, 0x1F004F, 0x1F005F};
   long long int event_info_val[]   = {0xA00, 0xB00, 0xC00, 0xD00};
   long long int init_budget_val[]  = {0xFFFFFFFFFFFFFFFE, 0xFFFFFA000, 0xFFFFFB000, 0xFFFFFC000};
   long long int period_val[]       = {0x10};
   long long int timer;
 
+ 
+
+  int instr_spm[] = {0x06400093, 
+                     0x0c808113, 
+                     0x002081b3, 
+                     0x00108233, 
+                     0x40110133,
+                     0x00200393,
+                     0x0043a223,
+                     0x0043a283, 
+                     0x00000013, 
+                     0xfe000ee3, 
+                     0x00000013};
+
+ 
+
   printf("Hello PMU!\n");
   uart_wait_tx_done();
+
+ 
 
   printf("Counter\n");
   write_counters(NUM_COUNTER, PMU_COUNTER_BASE_ADDR, COUNTER_WIDTH, counter_val);
@@ -58,17 +96,25 @@ int main(int argc, char const *argv[]) {
   printf("Period Register\n");
   write_counters(1, PMU_PERIOD_REG_BASE_ADDR, COUNTER_WIDTH, period_val);
 
+ 
+
   printf("Counters initialized!\n");
   uart_wait_tx_done();
 
+ 
+
   volatile uint32_t comp_array[NUM_ELEMENT] = {0};
   for (int i=0; i<NUM_ELEMENT; i++) {
-    comp_array[i] = comp_array[i] + i;
+     comp_array[i] = comp_array[i] + i;
   }
+
+ 
 
   printf("Array traversed!\n");
   uart_wait_tx_done();
-   unsigned int val_64;
+  unsigned int val_64;
+
+ 
 
   printf("Counter\n");
   read_counters(NUM_COUNTER, PMU_COUNTER_BASE_ADDR, COUNTER_WIDTH);
@@ -83,10 +129,28 @@ int main(int argc, char const *argv[]) {
   printf("Timer Register\n");
   read_counters(1, PMU_TIMER_BASE_ADDR, TIMER_WIDTH);
 
+ 
+
+  int NUM_INSTR = sizeof(instr_spm) / sizeof(instr_spm[0]);
+
+ 
+
+  // printf("Writing Instruction SPM\n");
+  // write_counters(NUM_INSTR, PMU_INSTR_SPM_BASE_ADDR, INSTR_WIDTH, instr_spm);
+
+ 
+
+  // printf("Reading Instruction SPM\n");
+  // read_counters(NUM_INSTR, PMU_INSTR_SPM_BASE_ADDR, INSTR_WIDTH);
+
+ 
+
   printf("The test is over!\n");
   uart_wait_tx_done();
   return 0;
 }
+
+ 
 
 void read_counters(int num_counter, long long unsigned int BASE_ADDR, int REG_SIZE_IN_BYTES) {
   // Make sure the read variable is 32bit.
@@ -100,6 +164,8 @@ void read_counters(int num_counter, long long unsigned int BASE_ADDR, int REG_SI
   }
 }
 
+ 
+
 void write_counters(int num_counter, long long unsigned int BASE_ADDR, int REG_SIZE_IN_BYTES, long long val[]) {
   // The AXI4-Lite interconnect is 32bit.
   // Can only write to 32bits at a time.
@@ -109,6 +175,8 @@ void write_counters(int num_counter, long long unsigned int BASE_ADDR, int REG_S
     } else if (REG_SIZE_IN_BYTES==8) {
       int val_l = val[i] & 0xFFFFFFFF;
       int val_h = val[i] >> 32;
+
+ 
 
       cfg_write(BASE_ADDR, val_l);
       cfg_write(BASE_ADDR+4, val_h);
