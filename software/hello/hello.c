c1bc14cd9274e35bd10b3eae51008b3fe32f0d0a
Arshaan-256 <arshaan256@gmail.com>
2023-10-13 15:10:32 -0400

    Updated hello.c for multicore non-coherent testing.

--- software/hello/hello.c
+++ software/hello/hello.c
@@ -1,10 +1,13 @@
 //#include "util.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "encoding.h"
 #include "utils.h"
 //#define FPGA_EMULATION
 
 int main(int argc, char const *argv[]) {
+
+  uint64_t mhartid;
 
   #ifdef FPGA_EMULATION
   int baud_rate = 9600;
@@ -15,16 +18,25 @@ int main(int argc, char const *argv[]) {
   int test_freq = 100000000;
   #endif  
   uart_set_cfg(0,(test_freq/baud_rate)>>4);
-  uint32_t * hyaxicfg_reg_mask = 0x1A101018;
-  pulp_write32(hyaxicfg_reg_mask,26); //128MB addressable
-  uint32_t * hyaxicfg_reg_memspace_end_addr1 = 0x1A10102C;
-  pulp_write32(hyaxicfg_reg_memspace_end_addr1,0x88000000);
-  uint32_t * hyaxicfg_reg_memspace_start_addr1 = 0x1A101028;
-  pulp_write32(hyaxicfg_reg_memspace_start_addr1,0x84000000);
-  uint32_t * hyaxicfg_reg_memspace_end_addr0 = 0x1A101024;
-  pulp_write32(hyaxicfg_reg_memspace_end_addr0,0x84000000); 
-  printf("Hello CVA6!\n");
-  uart_wait_tx_done();
+  
+  asm volatile (
+    "csrr %0, mhartid\n"
+    : "=r" (mhartid)
+  );
+
+  if (mhartid == 0) {
+    printf("Hello CVA6-0!\n");
+    uart_wait_tx_done();
+  } else if (mhartid == 1) {
+    printf("Hello CVA6-1!\n");
+    uart_wait_tx_done();
+  } else if (mhartid == 2) {
+    printf("Hello CVA6-2!\n");
+    uart_wait_tx_done();
+  } else if (mhartid == 3) {
+    printf("Hello CVA6-3!\n");
+    uart_wait_tx_done();
+  }
   return 0;
 }
  
