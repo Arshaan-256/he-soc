a12654d1e8ac41e0132199c7eb5d32ed074d7aa9
Arshaan-256 <arshaan256@gmail.com>
2023-12-03 15:36:51 -0500

    Updated software/pmu_test/pmu_defines.h Moved SPUs to outputs of CVA6 cores.

--- software/pmu_test/pmu_defines.h
+++ software/pmu_test/pmu_defines.h
@@ -73,57 +73,63 @@
 /// Defines for Core to/from LLC
 /// ****************************
 // Read requests from Core X to LLC
-#define LLC_RD_REQ          0x1F001F
-#define LLC_RD_REQ_CORE_0   0x1F4F1F
-#define LLC_RD_REQ_CORE_1   0x1F5F1F
-#define LLC_RD_REQ_CORE_2   0x1F6F1F
-#define LLC_RD_REQ_CORE_3   0x1F7F1F
+#define LLC_RD_REQ          0x00001F
+#define LLC_RD_REQ_CORE_0   0x2F001F
+#define LLC_RD_REQ_CORE_1   0x3F001F
+#define LLC_RD_REQ_CORE_2   0x4F001F
+#define LLC_RD_REQ_CORE_3   0x5F001F
 // Read responses to Core X from LLC
-#define LLC_RD_RES          0x1F003F
-#define LLC_RD_RES_CORE_0   0x1F4F3F
-#define LLC_RD_RES_CORE_1   0x1F5F3F
-#define LLC_RD_RES_CORE_2   0x1F6F3F
-#define LLC_RD_RES_CORE_3   0x1F7F3F
+#define LLC_RD_RES          0x00003F
+#define LLC_RD_RES_CORE_0   0x2F003F
+#define LLC_RD_RES_CORE_1   0x3F003F
+#define LLC_RD_RES_CORE_2   0x4F003F
+#define LLC_RD_RES_CORE_3   0x5F003F
 // Write requests from Core X to LLC
-#define LLC_WR_REQ          0x1F002F
-#define LLC_WR_REQ_CORE_0   0x1F4F2F
-#define LLC_WR_REQ_CORE_1   0x1F5F2F
-#define LLC_WR_REQ_CORE_2   0x1F6F2F
-#define LLC_WR_REQ_CORE_3   0x1F7F2F
+#define LLC_WR_REQ          0x00002F
+#define LLC_WR_REQ_CORE_0   0x2F002F
+#define LLC_WR_REQ_CORE_1   0x3F002F
+#define LLC_WR_REQ_CORE_2   0x4F002F
+#define LLC_WR_REQ_CORE_3   0x5F002F
 // Write responses to Core X from LLC
-#define LLC_RD_RES          0x1F004F
-#define LLC_WR_RES_CORE_0   0x1F4F4F
-#define LLC_WR_RES_CORE_1   0x1F5F4F
-#define LLC_WR_RES_CORE_2   0x1F6F4F
-#define LLC_WR_RES_CORE_3   0x1F7F4F
+#define LLC_RD_RES          0x00004F
+#define LLC_WR_RES_CORE_0   0x2F004F
+#define LLC_WR_RES_CORE_1   0x3F004F
+#define LLC_WR_RES_CORE_2   0x4F004F
+#define LLC_WR_RES_CORE_3   0x5F004F
 
 /// ***********************************
 /// Defines for LLC to/from Main Memory
 /// ***********************************
-// Read requests from Core X to MEM
-#define MEM_RD_REQ          0x2F001F
-#define MEM_RD_REQ_CORE_0   0x2F4F1F
-#define MEM_RD_REQ_CORE_1   0x2F5F1F
-#define MEM_RD_REQ_CORE_2   0x2F6F1F
-#define MEM_RD_REQ_CORE_3   0x2F7F1F
-// Read responses to Core X from MEM
-#define MEM_RD_RES          0x2F003F
-#define MEM_RD_RES_CORE_0   0x2F4F3F
-#define MEM_RD_RES_CORE_1   0x2F5F3F
-#define MEM_RD_RES_CORE_2   0x2F6F3F
-#define MEM_RD_RES_CORE_3   0x2F7F3F
-// Write requests from Core X to MEM
-#define MEM_WR_REQ          0x2F002F
-#define MEM_WR_REQ_CORE_0   0x2F4F2F
-#define MEM_WR_REQ_CORE_1   0x2F5F2F
-#define MEM_WR_REQ_CORE_2   0x2F6F2F
-#define MEM_WR_REQ_CORE_3   0x2F7F2F
-// Write responses to Core X from MEM
-#define MEM_WR_RES          0x2F004F
-#define MEM_WR_RES_CORE_0   0x2F4F4F
-#define MEM_WR_RES_CORE_1   0x2F5F4F
-#define MEM_WR_RES_CORE_2   0x2F6F4F
-#define MEM_WR_RES_CORE_3   0x2F7F4F
+/// Port ID and Mask | Source ID and Mask | Event ID and Mask
+///     LLC:0        |     4, 5, 6, 7     |    REQ: 1, 2 
+///     MEM:1        |                    |    RES: 3, 4
+///  ________________|____________________|_____ RD, WR ______
+
+// Read and write requests of all cores to Main Memory from LLC
+#define MEM_RD_REQ   0x1F001F  
+#define MEM_WR_REQ   0x1F002F
+// Read and write responses of all cores to LLC from Main Memory
+#define MEM_RD_RES   0x1F003F  
+#define MEM_WR_RES   0x1F004F
+
+// Read and write requests of Core X to Main Memory from LLC
+#define MEM_RD_REQ_CORE_0  0x1F4F1F
+#define MEM_RD_REQ_CORE_1  0x1F5F1F
+#define MEM_RD_REQ_CORE_2  0x1F6F1F
+#define MEM_RD_REQ_CORE_3  0x1F7F1F
+#define MEM_WR_REQ_CORE_0  0x1F4F2F
+#define MEM_WR_REQ_CORE_1  0x1F5F2F
+#define MEM_WR_REQ_CORE_2  0x1F6F2F
+#define MEM_WR_REQ_CORE_3  0x1F7F2F
+// Read and write responses of Core X to LLC from Main Memory
+#define MEM_RD_RES_CORE_0  0x1F4F3F
+#define MEM_RD_RES_CORE_1  0x1F5F3F
+#define MEM_RD_RES_CORE_2  0x1F6F3F
+#define MEM_RD_RES_CORE_3  0x1F7F3F
+#define MEM_WR_RES_CORE_0  0x1F4F4F
+#define MEM_WR_RES_CORE_1  0x1F5F4F
+#define MEM_WR_RES_CORE_2  0x1F6F4F
+#define MEM_WR_RES_CORE_3  0x1F7F4F
 
 /// **********************************************************************
 /// Defines for Event Info Register
