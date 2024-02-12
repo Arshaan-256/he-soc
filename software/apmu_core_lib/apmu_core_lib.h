5bb2582fdad7985e6ce36a50471c209bcac1be27
Arshaan-256 <arshaan256@gmail.com>
2024-02-12 02:56:00 -0500

    Added defines for 32b multiplication and 64-32b addition.

--- software/apmu_core_lib/apmu_core_lib.h
+++ software/apmu_core_lib/apmu_core_lib.h
@@ -3,6 +3,19 @@
 #define write_32b(addr, val_)  (*(volatile int *)(long)(addr) = val_)
 
 // Macros for custom PMU core instructions.
+// Counter read: rd = cnt[rs1]
 #define counter_read(rd, rs1)       asm volatile ("cnt.rd\t%0,%1" : "=r" (rd) : "r" (rs1));
+// Counter write: cnt[rs1] = rs2
 #define counter_write(rs1, rs2)     asm volatile ("cnt.wr\t%0,%1" :: "r" (rs1), "r" (rs2));
 
+#define mult_32u_32u(in1, in2, out_l, out_u) {\
+    asm volatile ("mul %0,%1,%2" : "=r" (out_l) : "r"(in1), "r"(in2)); \
+    asm volatile ("mulhu %0,%1,%2" : "=r" (out_u) : "r"(in1), "r"(in2)); \
+}
+
+#define add_64u_32u(in1_l, in1_u, in2, out_l, out_u) {\
+    asm volatile ("# out = in1 + in2"); \
+    out_l = in1_l + in2; \
+    if (out_l < (unsigned) in1_l) \
+        out_u = in1_u + 1; \
+}
